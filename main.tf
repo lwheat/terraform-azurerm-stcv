276b3f6937b9d81fd36a74cf54b15a6648ac6a95
Lee Wheat <lee.wheat@spirent.com>
2024-10-08 15:04:52 -0400

    update accelerated networking keyword

--- main.tf
+++ main.tf
@@ -158,11 +158,11 @@ resource "azurerm_network_interface" "mgmt_plane" {
 }
 
 resource "azurerm_network_interface" "test_plane" {
-  count                         = var.instance_count
-  name                          = "nic-test-${var.instance_name}-${count.index}"
-  location                      = var.resource_group_location
-  resource_group_name           = var.resource_group_name
-  enable_accelerated_networking = var.enable_accelerated_networking
+  count                          = var.instance_count
+  name                           = "nic-test-${var.instance_name}-${count.index}"
+  location                       = var.resource_group_location
+  resource_group_name            = var.resource_group_name
+  accelerated_networking_enabled = var.enable_accelerated_networking
 
   ip_configuration {
     name                          = "ipc-test-${var.instance_name}-${count.index}"
